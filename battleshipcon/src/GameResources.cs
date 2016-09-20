f25ffc67547dca4b5757f136017fd55b00bc2081
bendg <100498278@student.swin.edu.au>
2016-09-21 09:45:40 +1000

    aaaalllll

--- battleshipcon/src/GameResources.cs
+++ battleshipcon/src/GameResources.cs
@@ -69,7 +69,7 @@ public static class GameResources
 	/// </summary>
 	private static void LoadMusic()
 	{
-		NewMusic("Background", "horrordrone.mp3");
+		NewMusic("Background", "rising_tension.mp3");
 	}
 
 	/// <summary>
