bc424deac76764d2efe503c1b9516b4a05d230a2
Gurusha95 <gurusha.rana@hotmaiol.com>
2016-09-21 11:08:38 +1000

    change the font and size of menu

--- battleshipcon/src/GameResources.cs
+++ battleshipcon/src/GameResources.cs
@@ -18,7 +18,7 @@ public static class GameResources
 		NewFont("ArialLarge", "arial.ttf", 80);
 		NewFont("Courier", "cour.ttf", 14);
 		NewFont("CourierSmall", "cour.ttf", 8);
-		NewFont("Menu", "ffaccess.ttf", 8);
+		NewFont("Menu", "Chunkfive.otf", 14);
 	}
 	
 	/// <summary>
