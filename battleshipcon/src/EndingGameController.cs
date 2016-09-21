63b4716ff3703fef18dc8615662622a0e345b1d9
Simon <1628623@student.swin.edu.au>
2016-09-21 11:23:18 +1000

    changes to win and loss screens p1

--- battleshipcon/src/EndingGameController.cs
+++ battleshipcon/src/EndingGameController.cs
@@ -24,9 +24,9 @@ static class EndingGameController
         UtilityFunctions.DrawSmallField(GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer);
 
 		if (GameController.HumanPlayer.IsDestroyed) {
-			SwinGame.DrawTextLines("YOU LOSE!", Color.White, Color.Transparent, GameResources.GameFont("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth(), SwinGame.ScreenHeight());
+			SwinGame.DrawBitmap(GameResources.GameImage("loser"), 0, SwinGame.ScreenHeight()/2-113
 		} else {
-			SwinGame.DrawTextLines("-- WINNER --", Color.White, Color.Transparent, GameResources.GameFont("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth(), SwinGame.ScreenHeight());
+			SwinGame.DrawBitmap(GameResources.GameImage("winner"), 0, SwinGame.ScreenHeight()/2-113);
 		}
 	}
 
